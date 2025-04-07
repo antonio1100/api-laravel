74677ef64ef9bca90b0ae12fc24fe238850dc789
unknown <toluca_10_34@hotmail.com>
2025-04-07 03:05:29 -0600

    primeros cambios

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Domicilio;
+use Carbon\Carbon;
+
+class UserController extends Controller
+{
+    public function index(){
+        $usuarios_bd = Domicilio::with('usuario')->get();
+
+        foreach ($usuarios_bd as $key => $value) {
+            # code...
+            // dd($value->usuario);
+         
+            $edad = Carbon::createFromDate($value->usuario->fecha_nacimiento)->age;
+
+            $datos[] = [
+                'id' => $value->usuario->id,
+                'nombre_usuario' => $value->usuario->name,
+                'fecha_nacimiento' => $value->usuario->fecha_nacimiento,
+                'edad' => $edad,
+                'domicilio_completo' => $value->domicilio.' '.$value->numero_ext.' '.$value->colonia.' '.$value->codigo_postal.' '.$value->ciudad
+        
+                ];
+      
+        }
+
+       
+
+        return $datos;
+    }
+
+    public function show($id){
+        $show_producto = User::find($id);
+
+        return $show_producto;
+    }
+
+    public function delete($id){
+        // return $id;
+        $producto = User::destroy($id);
+        return $producto;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::controller(UserController::class)->group(function () {
+    // Route::get('/users','UserController@index')->name('productos.index');
+    Route::get('/users','index')->name('users.index');
+    // Route::post('/producto','store')->name('producto.store');
+    Route::get('/producto/{id}','show')->name('producto.show');
+    // Route::put('/producto/{id}','update')->name('producto.update');
+    Route::delete('/producto/{id}','delete')->name('producto.delete');
+});
