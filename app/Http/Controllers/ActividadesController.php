d2d6193502cd88b3e2fdc799cde710541bee3f60
antonio1100 <antonioavilaf978@gmail.com>
2025-04-09 13:36:10 -0600

    ultimos cambios para funcionalidad crud tareas

--- app/Http/Controllers/ActividadesController.php
+++ app/Http/Controllers/ActividadesController.php
@@ -14,12 +14,12 @@ class ActividadesController extends Controller
 
     public function store(Request $request){
         $new_task = new Task();
-        $new_task->categoria = $request->category;
-        $new_task->titulo = $request->title;
+        $new_task->categoria = $request->categoria;
+        $new_task->titulo = $request->titulo;
         $new_task->estado = $request->status;
-        $new_task->descripcion = $request->description;
-        $new_task->fecha_de_vencimiento = $request->dueDate;
-        $new_task->prioridad = $request->priority;
+        $new_task->descripcion = $request->descripcion;
+        $new_task->fecha_de_vencimiento = $request->fecha_de_vencimiento;
+        $new_task->prioridad = $request->prioridad;
         $new_task->save();
 
         return $new_task;
@@ -32,14 +32,15 @@ class ActividadesController extends Controller
 
     public function update(Request $request, $id)
     {
+        // return $request;
         $upd_task = Task::findOrFail($id);
 
-        $upd_task->categoria = $request->category;
-        $upd_task->titulo = $request->title;
+        $upd_task->categoria = $request->categoria;
+        $upd_task->titulo = $request->titulo;
         $upd_task->estado = $request->status;
-        $upd_task->descripcion = $request->description;
-        $upd_task->fecha_de_vencimiento = $request->dueDate;
-        $upd_task->prioridad = $request->priority;
+        $upd_task->descripcion = $request->descripcion;
+        $upd_task->fecha_de_vencimiento = $request->fecha_de_vencimiento;
+        $upd_task->prioridad = $request->prioridad;
         $upd_task->save();
 
         return $upd_task;
